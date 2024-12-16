09d6ee91728da19aa8bdd86f1d9727f614903247
wsfer <93054415+wsfer@users.noreply.github.com>
2024-12-16 15:55:22 -0300

    feat: add initial files and configs

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index', { message: 'message' }));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
