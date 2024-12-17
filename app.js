718085c7ae1f03a6b62f99c6ca2d57729ec499c2
wsfer <93054415+wsfer@users.noreply.github.com>
2024-12-17 14:04:39 -0300

    feat: add the two main routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index', { message: 'message' }));
+app.get('/new', (req, res) => res.send('Message form'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
