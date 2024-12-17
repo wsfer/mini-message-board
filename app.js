32a43b8c35b80e38cf91c60b09759e827b7c06b1
wsfer <93054415+wsfer@users.noreply.github.com>
2024-12-17 15:28:29 -0300

    feat: add logic to serve static files

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const messages = [
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
