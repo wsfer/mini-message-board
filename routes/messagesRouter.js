b0baac2c5acc6f45672d0fb2e984036af9071bbe
wsfer <93054415+wsfer@users.noreply.github.com>
2024-12-20 15:32:39 -0300

    feat: add page not found route on messages router

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 const messagesController = require('../controllers/messagesController');
+const NotFoundError = require('../errors/NotFoundError');
+
 const messagesRouter = Router();
 
 messagesRouter.get('/', messagesController.getAllMessages);
 messagesRouter.get('/message/:id', messagesController.getMessage);
 messagesRouter.get('/new', messagesController.getMessageForm);
 messagesRouter.post('/new', messagesController.postMessage);
+messagesRouter.get('*', (req, res) => {
+  throw new NotFoundError('Page not found');
+});
 
 module.exports = messagesRouter;
