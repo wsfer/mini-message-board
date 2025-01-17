8b6621fe3522c92de6b497a16a21c91f59e94285
wsfer <93054415+wsfer@users.noreply.github.com>
2025-01-16 22:43:50 -0300

    feat: create database query functions

---
+++ db/queries.js
@@ -0,0 +1,20 @@
+const pool = require('./pool');
+
+async function getAllMessages() {
+  const { rows } = await pool.query('SELECT * FROM messages');
+  return rows;
+}
+
+async function getMessageById(id) {
+  const { rows } = await pool.query('SELECT * FROM messages WHERE id=$1', [id]);
+  return rows[0];
+}
+
+async function createMessage({ username, text }) {
+  await pool.query('INSERT INTO messages (username, text) VALUES ($1, $2)', [
+    username,
+    text,
+  ]);
+}
+
+module.exports = { getAllMessages, getMessageById, createMessage };
