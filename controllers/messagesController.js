30da11c1fcd1591c3d32aef1c21ed693ff35238b
wsfer <93054415+wsfer@users.noreply.github.com>
2025-01-16 22:45:31 -0300

    feat: change from old memory based fake database to postgresql

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../db/queries');
 const asyncHandler = require('express-async-handler');
 const NotFoundError = require('../errors/NotFoundError');
 
